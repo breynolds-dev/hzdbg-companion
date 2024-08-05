a3692add1cbc21001585a48aac19c4c05694cd35
Brian Reynolds <bri.reynolds@gmail.com>
2024-08-04 19:33:25 -0500

    :hammer: chore: add eslint and configs

---
+++ eslint/node.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    './shared/core.js',
+    './shared/typescript.js',
+    './shared/prettier.js'
+  ],
+  plugins: [
+    'node'
+  ],
+  env: {
+    node: true
+  },
+  rules: {
+    'no-buffer-constructor': 'warn',
+    'node/no-path-concat': 'warn',
+  },
+};
